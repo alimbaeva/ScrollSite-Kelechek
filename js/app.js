c1c778ed2090d368e2e4da0e480e37cf4e845cc2
Asel Alimbaeva <asel_19.86@mail.ru>
2021-11-13 09:54:17 +0300

    added code str 137 to 162

--- js/app.js
+++ js/app.js
@@ -133,3 +133,30 @@ scrollLinks.forEach(function (link) {
         linksContainer.style.height = 0;
     });
 });
+
+//=================
+//about
+//=================
+
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function (e) {
+    const id = e.target.dataset.id;
+
+    if (id) {
+        btns.forEach(function (btn) {
+            btn.classList.remove('active');
+        });
+
+        e.target.classList.add('active');
+
+        articles.forEach(function (article) {
+            article.classList.remove('active');
+        });
+
+        const tabConent = document.getElementById(id);
+        tabConent.classList.add('active');
+    }
+});
